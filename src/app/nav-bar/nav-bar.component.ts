9a28731e0e4fee508f90b2a51fe9a91826a37719
corsantic <aydofurkan25@gmail.com>
2017-11-20 15:58:49 +0300

    clean code

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,17 +1,13 @@
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase';
-import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent   {
-  user$: Observable <firebase.User>;
 
-  constructor(private auth:AuthService) { 
+  constructor(public auth:AuthService) { 
  
     
   }
