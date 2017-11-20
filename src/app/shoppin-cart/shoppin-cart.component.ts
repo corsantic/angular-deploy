05e5ee792e99f93baad75a147fcabfc6d2e98702
corsantic <aydofurkan25@gmail.com>
2017-11-20 14:24:00 +0300

    add components

---
+++ src/app/shoppin-cart/shoppin-cart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shoppin-cart',
+  templateUrl: './shoppin-cart.component.html',
+  styleUrls: ['./shoppin-cart.component.css']
+})
+export class ShoppingCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
