acbbcd7a0895bd796ee912c95b543746c84d448a
corsantic <aydofurkan25@gmail.com>
2017-11-20 10:59:07 +0300

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDG_biuzk9IVvxB0x-lTquF1f-SbqNP0W4",
+    authDomain: "oshop-b4c32.firebaseapp.com",
+    databaseURL: "https://oshop-b4c32.firebaseio.com",
+    projectId: "oshop-b4c32",
+    storageBucket: "",
+    messagingSenderId: "481472875631"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+        apiKey: "AIzaSyDG_biuzk9IVvxB0x-lTquF1f-SbqNP0W4",
+        authDomain: "oshop-b4c32.firebaseapp.com",
+        databaseURL: "https://oshop-b4c32.firebaseio.com",
+        projectId: "oshop-b4c32",
+        storageBucket: "",
+        messagingSenderId: "481472875631"
+  }
 };
