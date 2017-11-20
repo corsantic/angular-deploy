24bb8543b8ddc9779086dbfffebd791efe2e95ce
corsantic <aydofurkan25@gmail.com>
2017-11-20 10:33:13 +0300

    initialize for new project

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { DeployDemoPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('deploy-demo App', () => {
-  let page: DeployDemoPage;
+describe('oshop App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new DeployDemoPage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
 import { browser, by, element } from 'protractor';
 
-export class DeployDemoPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
