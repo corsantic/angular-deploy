82fb863772707b588f0dc8fa1cb6dd472fcb5778
corsantic <aydofurkan25@gmail.com>
2017-11-21 13:00:11 +0300

    editing and bootstrap card preview

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,3 +1,4 @@
+import { ProductService } from './../../product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent  {
 
-  constructor() { }
+  products$;
+  
+  constructor(private productService:ProductService) { 
 
-  ngOnInit() {
+  this.products$=this.productService.getAll();
   }
 
 }
