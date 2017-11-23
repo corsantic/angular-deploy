d03f76bd6c0adda3e12f4ed3cbfc2a5d2ede6fd8
corsantic <aydofurkan25@gmail.com>
2017-11-23 15:09:06 +0300

    implementing quantity change to buttons

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -17,17 +17,20 @@ export class ProductCardComponent {
   constructor(private cartService: ShoppingCartService) { }
 
 
-  addToCart(product:Product)
-  {
-    this.cartService.addToCart(product);
+  addToCart() {
+    this.cartService.addToCart(this.product);
   }
 
-  getQuantity(){
 
-    if(!this.shoppingCart) return 0;
-    
+removeFromCart(){
+  this.cartService.removeFromCart(this.product);
+}
+
+  getQuantity() {
+
+    if (!this.shoppingCart) return 0;
+
     let item = this.shoppingCart.items[this.product.$key];
-
     return item ? item.quantity : 0;
 
   }
--- src/app/shopping-card.service.ts
+++ src/app/shopping-card.service.ts
@@ -17,16 +17,15 @@ export class ShoppingCartService {
 
   async getCart() {
 
-    let cartId= await this.getOrCreateCartId();
+    let cartId = await this.getOrCreateCartId();
 
     return this.db.object('/shopping-carts/' + cartId);
   }
-
   private getItem(cartId: string, productId: string) {
     return this.db.object('shopping-carts/' + cartId + '/items/' + productId);
   }
 
-  private async getOrCreateCartId() :Promise<string> {
+  private async getOrCreateCartId(): Promise<string> {
 
     let cartId = localStorage.getItem('cartId');
     if (cartId) return cartId;
@@ -35,15 +34,33 @@ export class ShoppingCartService {
 
     localStorage.setItem('cartId', result.key);
     return result.key;
- }
+  }
+
 
   async addToCart(product: Product) {
+
+    this.updateItemQuantity(product, 1);
+  }
+
+
+  async removeFromCart(product: Product) {
+
+    this.updateItemQuantity(product, -1);
+  }
+
+
+
+
+
+
+  private async updateItemQuantity(product: Product, change: number) {
 
     let cartId = await this.getOrCreateCartId();
     let item$ = this.getItem(cartId, product.$key);
 
     item$.take(1).subscribe(item => {
-      item$.update({ product: product, quantity: (item.quantity || 0) + 1 });
+      item$.update({ product: product, quantity: (item.quantity || 0) + change });
     });
+
   }
 }
