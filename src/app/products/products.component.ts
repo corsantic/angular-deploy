dbbe1ecf8c38c30d4038ad05765529d94f33eace
corsantic <aydofurkan25@gmail.com>
2017-11-23 10:18:31 +0300

    all products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../category.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
+import 'rxjs/add/operator/switchMap';
 
 
 @Component({
@@ -10,29 +11,37 @@ import { Product } from '../models/product';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent  {
-products:Product[]=[];
-filteredProducts:Product[]=[];
-categories$;
-category:string;
+export class ProductsComponent {
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  categories$;
+  category: string;
 
   constructor(
-    route:ActivatedRoute,
-    productService:ProductService,
-    categoryService:CategoryService) { 
-      productService.getAll().subscribe(products => this.products = products);
-    this.categories$=categoryService.getAll();
+    route: ActivatedRoute,
+    productService: ProductService,
+    categoryService: CategoryService) {
+      
+    productService
+      .getAll().switchMap(products => {
+        this.products = products
+        return route.queryParamMap;
+      }).subscribe(params => {
+        this.category = params.get('category');
 
-    route.queryParamMap.subscribe(params=>{
-      this.category=params.get('category');
 
-    this.filteredProducts=(this.category)?
-      this.products.filter(p=> p.category === this.category):
-      this.products;
-    });
+        this.filteredProducts = (this.category) ?
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+      });
+    this.categories$ = categoryService.getAll();
   }
 
 
 
-
 }
+
+
+
+
+
