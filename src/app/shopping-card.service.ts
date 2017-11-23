df6ef521af8fb5d92424bf1ecf7b58f261a8b1ff
corsantic <aydofurkan25@gmail.com>
2017-11-23 13:22:04 +0300

    refactoring

--- src/app/shopping-card.service.ts
+++ src/app/shopping-card.service.ts
@@ -1,14 +1,40 @@
+import { async } from '@angular/core/testing';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ShoppingCartService {
 
-  constructor(private db:AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase) { }
 
-  create(){
+  private create() {
     return this.db.list('/shopping-carts').push({
-      dateCreated:new Date().getTime()
+      dateCreated: new Date().getTime()
     });
   }
+
+  private getCart(cartId: string) {
+    return this.db.object('/shopping-carts/' + cartId);
+  }
+
+
+
+  private async getOrCreateCart() {
+
+    let cartId = localStorage.getItem('cartId');
+    if (!cartId) {
+      let result = await this.create();
+
+      localStorage.setItem('cartId', result.key);
+      return this.getCart(result.key);
+
+    }
+    
+    return this.getCart(cartId);
+
+
+  }
+
+
+
 }
