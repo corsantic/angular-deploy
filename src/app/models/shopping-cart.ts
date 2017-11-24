4369530795b96accbfa2973eced8569ddf01e0d8
corsantic <aydofurkan25@gmail.com>
2017-11-24 10:27:04 +0300

    fixing bugs and adding templates

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -7,16 +7,21 @@ export class ShoppingCart {
 
     constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
 
+        this.itemsMap=itemsMap || {};
+        
         for (let productId in itemsMap) {
             let item = itemsMap[productId];
-
-            this.items.push(new ShoppingCartItem(item.product, item.quantity));
+            let x=new ShoppingCartItem();
+            Object.assign(x,item);
+            x.$key=productId;
+            this.items.push(x);
 
         }
     }
 
 
     getQuantity(product:Product) {
+
         let item = this.itemsMap[product.$key];
         
         return item ? item.quantity : 0;
